f351030182b4564523e833b4782f9e69dec6f9c1
G1OJS <140509227+G1OJS@users.noreply.github.com>
2025-08-22 22:59:21 +0100

    Develop plotting algorithms

--- readall.py
+++ readall.py
@@ -1,6 +1,11 @@
 
 import datetime
 
+def get_session_info_string(sess):
+    ts, te, bm = sess
+    tmins = (te-ts)/60
+    return(f"Session {datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H%M')} {bm} for {tmins} mins")
+
 def allstr_to_epoch(s):
     dt = datetime.datetime(int("20"+s[0:2]), int(s[2:4]), int(s[4:6]),
                             int(s[7:9]), int(s[9:11]), int(s[11:13]))
@@ -11,7 +16,7 @@ def todict(line):
     if len(vals) < 9:
         return False
     t = allstr_to_epoch(vals[0])
-    return {'t': t, 'ts': vals[0], 'bm':vals[1].split(".")[0]+vals[3], 'oc': vals[8], 'rp': int(vals[4])}
+    return {'t': int(t), 'ts': vals[0], 'bm':vals[1].split(".")[0]+vals[3], 'oc': vals[8], 'rp': int(vals[4])}
 
 def get_decodes(fp):
     decodes =[]
@@ -24,17 +29,19 @@ def get_decodes(fp):
 def get_single_file_sessions(decodes):
     tg = 5*60
     t0=0
+    bm0 = ""
     s_idx = []
     for idx, d in enumerate(decodes):
         t1 = int(d['t'])
-        if(t1-t0 > tg):
+        if(t1-t0 > tg or d['bm'] != bm0): # new session
             s_idx.append(idx)
         t0=t1
+        bm0 = d['bm']
     s_idx.append(len(decodes)-1)
     sess = []
     for i, idxs in enumerate(s_idx[0:-2]):
         idxe = s_idx[i+1]
-        sess.append((decodes[idxs]['t'], decodes[idxe]['t']))
+        sess.append((int(decodes[idxs]['t']), int(decodes[idxe]['t']), decodes[idxs]['bm']))
     return sess
 
 def read_allfile(fp):
@@ -46,26 +53,21 @@ def get_overlapping_sessions(a,b):
     ranges = []
     i = j = 0
     while i < len(a) and j < len(b):
-        a_left, a_right = a[i]
-        b_left, b_right = b[j]
-
+        a_left, a_right, a_bm = a[i]
+        b_left, b_right, b_bm = b[j]
         if a_right < b_right:
             i += 1
         else:
             j += 1
-
-        if a_right >= b_left and b_right >= a_left:
+        if a_right >= b_left and b_right >= a_left and a_bm == b_bm:
             end_pts = sorted([a_left, a_right, b_left, b_right])
-            middle = [end_pts[1], end_pts[2]]
+            middle = [end_pts[1], end_pts[2], a_bm]
             ranges.append(middle)
-
-    ri = 0
-    while ri < len(ranges)-1:
-        if ranges[ri][1] == ranges[ri+1][0]:
-            ranges[ri:ri+2] = [[ranges[ri][0], ranges[ri+1][1]]]
-
-        ri += 1
-
+#    ri = 0
+#    while ri < len(ranges)-1:
+#        if ranges[ri][1] == ranges[ri+1][0]:
+#            ranges[ri:ri+2] = [[ranges[ri][0], ranges[ri+1][1]]]
+#        ri += 1    
     return ranges
 
 
--- run.py
+++ run.py
@@ -1,18 +1,18 @@
 import matplotlib.pyplot as plt
-from readall import read_allfile, get_overlapping_sessions
+import os
+from readall import read_allfile, get_overlapping_sessions, get_session_info_string
+
 
 def time_window_decodes(decodes, tmin, tmax):
     decs = []
     for d in decodes:
-        if (d['ts'] in range(int(tmin), int(tmax))):
+        if (d['t']>= tmin and d['t']<= tmax):
             decs.append(d)
     return decs
 
-def plot_session(decA, decB, ts, te):
+def plot_session(decA, decB, ts, te, plotfile):
     fig,ax = plt.subplots()
     ax.axline((0, 0), slope=1, color="black", linestyle=(0, (5, 5)))
-   # plt.ion()
-    timewin_secs = 7*60
 
     from random import randint
     colors = []
@@ -21,50 +21,71 @@ def plot_session(decA, decB, ts, te):
         colors.append('#%06X' % randint(0, 0xFFFFFF))
 
     calls = set()
+    bandModes = set()
+    dA, dB = [], []
     for d in decA:
-        if(d['t'] in range(ts,te)):
+        if(d['t']>=ts and d['t']<=te):
             calls.add(d['oc'])
+            bandModes.add(d['bm'])
+            dA.append(d)
     print(f"{len(calls)} callsigns from all A")
     for d in decB:
-        if(d['t'] in range(ts,te)):
+        if(d['t']>=ts and d['t']<=te):
             calls.add(d['oc'])
+            bandModes.add(d['bm'])
+            dB.append(d)
     print(f"{len(calls)} callsigns from all A and all B")
 
+    # plotfile = plotfile + " "+ ",".join(list(bandModes))+".png"
+    nReports = 0
     for i, c in enumerate(calls):
+     #   if(i % 10 ==0):
+     #       print(f"At callsign {i} of {len(calls)}: accumulated {nReports} reports")
         series_x = []
         series_y = []
-        for da in decA:
+        for da in dA:
             if(da['oc']==c):
-                for db in decB:
-                    if(db['oc'] == c and db['t'] == d['t']): # coincident reports same callsign
+                for db in dB:
+                    if(db['oc'] == c and abs(da['t'] - db['t']) <30): # coincident reports same callsign
                         series_x.append(da['rp'])
                         series_y.append(db['rp'])
-                ax.plot(series_x, series_y, color = colors[i % ncolors], marker ="o")
-    plt.show()
+        ax.plot(series_x, series_y, color = colors[i % ncolors], marker ="o")
+        nReports += len(series_x)
+    if(nReports>0):
+        plt.tight_layout()
+        if not os.path.exists("plots"):
+            os.makedirs("plots")
+        print(f"Saving plot plots/{plotfile}")
+        plt.savefig(f"plots/{plotfile}")
+    plt.close()
 
-     
 allA = r"C:\Users\drala\AppData\Local\WSJT-X\all.txt"
 allB = r"C:\Users\drala\AppData\Local\WSJT-X - AltAB\all.txt"
 
 print("Reading all files")
 alldecA, sA = read_allfile(allA)
+for s in sA:
+    print(f"A: {get_session_info_string(s)}")
 alldecB, sB = read_allfile(allB)
+for s in sB:
+    print(f"B: {get_session_info_string(s)}")
 
 print("Finding overlapping sessions")
 coinc = get_overlapping_sessions(sA,sB)
-
+for c in coinc:
+    print(f"AB: {get_session_info_string(c)}")
+    
 print("Time windowing decode lists")
-tmin, _ = coinc[0]
-_, tmax = coinc[-1]
+tmin, _, _ = coinc[0]
+_, tmax, _ = coinc[-1]
 decA = time_window_decodes(alldecA, tmin, tmax)
 decB = time_window_decodes(alldecB, tmin, tmax)
 
 print("Plotting sessions")
 for c in coinc:
-    ts, te = c
-    tmins = (te-ts)/60
-    if(tmins < 30):
-        print(f"Session {tmins} mins from {ts}")
-        plot_session(decA, decB, int(ts), int(te))
+    info_str = get_session_info_string(c)
+    ts, te, bm = c
+    print(f"\nPlotting {info_str}")
+    plot_session(decA, decB, int(ts), int(te), f"{info_str}.png")
 
 
